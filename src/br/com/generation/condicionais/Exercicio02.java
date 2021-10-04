530ab0ecef3c09cc2146f99b46c764a2d72d006e
Helen Lissa Sonoda <helen.lissa@gmail.com>
2021-10-04 12:32:44 -0300

    Ajuste da indentação

--- src/br/com/generation/condicionais/Exercicio02.java
+++ src/br/com/generation/condicionais/Exercicio02.java
@@ -29,23 +29,22 @@ public class Exercicio02 {
 		
 		if (a > b && b > c){
 			System.out.println("\nOs números em ordem crescente são: " + c + ", " + b + ", " + a);
-		            
-        } else if (a > c && c > b){
+		
+		} else if (a > c && c > b){
             System.out.println("\nOs números em ordem crescente são: " + b + ", " + c + ", " + a);
-            
-        } else if (b > a && a > c){
+        
+		} else if (b > a && a > c){
             System.out.println("\nOs números em ordem crescente são: " + c + ", " + a + ", " + b);
             
-        } else if (b > c && c > a){
+		} else if (b > c && c > a){
             System.out.println("\nOs números em ordem crescente são: " + a + ", " + c + ", " + b);
             
-        } else if (c > b && b > a){
+		} else if (c > b && b > a){
             System.out.println("\nOs números em ordem crescente são: " + a + ", " + b + ", " + c);
             
-        } else if (c > a && a > b){
+		} else if (c > a && a > b){
             System.out.println("\nOs números em ordem crescente são: " + b + ", " + a + ", " + c);
-        } 
-		
+        }
 		sc.close();
 	}
 
